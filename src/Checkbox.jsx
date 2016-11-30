4477ce8dbb5c71b98494a83b085d2d7c1ab56bf6
BingqiChen <bingqichen@live.cn>
2016-11-30 12:59:15 +0800

    修改代码格式

--- src/Checkbox.jsx
+++ src/Checkbox.jsx
@@ -63,8 +63,9 @@ export default class Checkbox extends React.Component {
   };
 
   handleChange = (e) => {
-    if (this.props.disabled) 
+    if (this.props.disabled) {
       return;
+    }
     if (!('checked' in this.props)) {
       this.setState({
         checked: e.target.checked,
