dd4d633e8e88fcc6a82ca96f48e42c5337da2936
kilya11 <kilya@ymail.com>
2019-10-23 09:49:43 +0200

    Test title prop
    https://github.com/react-component/checkbox/issues/104

--- tests/index.spec.jsx
+++ tests/index.spec.jsx
@@ -53,6 +53,12 @@ describe('rc-checkbox', () => {
     expect(renderedInput.attributes.value.value).toEqual('6');
   });
 
+  it('passes title prop to input', () => {
+    const wrapper = mount(<Checkbox title="my-custom-title" />);
+    const renderedInput = wrapper.find('input').instance();
+    expect(renderedInput.attributes.title.value).toEqual('my-custom-title');
+  });
+  
   it('focus()', () => {
     const container = document.createElement('div');
     document.body.appendChild(container);
