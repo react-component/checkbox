ac34c5d721b75887270c44e888e75739d78a57eb
wangxingkang <wang_xingkang@qq.com>
2022-12-08 10:39:29 +0800

    refactor: rewrite using typescript

---
+++ src/index.tsx
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import useMergedState from 'rc-util/lib/hooks/useMergedState';
+import classNames from 'classnames';
+
+export interface CheckboxChangeEvent {
+  target: CheckboxChangeEventTarget;
+  stopPropagation: () => void;
+  preventDefault: () => void;
+  nativeEvent: React.ChangeEvent<HTMLInputElement>['nativeEvent'];
+}
+
+export interface CheckboxChangeEventTarget extends CheckboxProps {
+  checked: boolean;
+}
+
+export interface CheckboxRef {
+  focus: () => void;
+  blur: () => void;
+  input: HTMLInputElement | null;
+}
+
+export interface CheckboxProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  prefixCls?: string;
+  onChange?: (e: CheckboxChangeEvent) => void;
+}
+
+export const Checkbox = forwardRef<CheckboxRef, CheckboxProps>((props, ref) => {
+  const {
+    prefixCls = 'rc-checkbox',
+    className,
+    style,
+    checked,
+    disabled,
+    defaultChecked = false,
+    type = 'checkbox',
+    onChange,
+    ...inputProps
+  } = props;
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [rawValue, setRawValue] = useMergedState(defaultChecked, {
+    value: checked,
+  });
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current?.focus();
+    },
+    blur: () => {
+      inputRef.current?.blur();
+    },
+    input: inputRef.current,
+  }));
+
+  const classString = classNames(prefixCls, className, {
+    [`${prefixCls}-checked`]: rawValue,
+    [`${prefixCls}-disabled`]: disabled,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (disabled) {
+      return;
+    }
+
+    const checked = e.target.checked;
+
+    if (!('checked' in props)) {
+      setRawValue(checked);
+    }
+
+    onChange?.({
+      target: {
+        ...props,
+        checked,
+      },
+      stopPropagation() {
+        e.stopPropagation();
+      },
+      preventDefault() {
+        e.preventDefault();
+      },
+      nativeEvent: e.nativeEvent,
+    });
+  };
+
+  return (
+    <span className={classString} style={style}>
+      <input
+        {...inputProps}
+        className={`${prefixCls}-input`}
+        ref={inputRef}
+        onChange={handleChange}
+        disabled={disabled}
+        checked={!!rawValue}
+        type={type}
+      />
+      <span className={`${prefixCls}-inner`} />
+    </span>
+  );
+});
+
+export default Checkbox;
